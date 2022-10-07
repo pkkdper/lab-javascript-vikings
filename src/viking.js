926ab227fa7541136725f2dcdb29ac94ade02f69
Oliwia Gierczak <oliwia.giercza@gmail.com>
2022-10-07 03:58:58 -0500

    done

--- src/viking.js
+++ src/viking.js
@@ -2,6 +2,12 @@
 class Soldier {constructor(health, strength) {
 this.health = health;
 this.strength = strength
+}
+attack() {
+    return this.strength;
+}
+receiveDamage(damage) {
+    this.health -= damage;
 }}
 
 // Viking
